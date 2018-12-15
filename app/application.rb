ff47da813507721ffe7ec0ccdcee720ddcf662f7
Jason Mei <jason.mei403@gmail.com>
2018-12-15 20:18:46 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,4 +1,4 @@
-class Application
+class Application < Item
 
   def call(env)
     resp = Rack::Response.new
@@ -12,7 +12,7 @@ class Application
       resp.write "Route not found"
       resp.status = 404
     end
-    
+
     resp.finish
   end
 
